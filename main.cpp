be47082193244b2508d238467caaf44bfb81f049
Serafim1979 <korolkov.ivan@mail.ru>
2019-07-02 21:42:34 +0400

    my testing commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+	char charr1[20];
+	char charr2[20] = "jaguar";
+	string str1;
+	string str2 = "panther";
+
+	cout << "Enter a kind of feline: ";
+	cin >> charr1;
+
+	cout << "Enter another kind of feline: ";
+	cin >> str1;
+
+	cout << "Here are some felines:\n";
+	cout << charr1 << " " << charr2 << " "
+		<< str1 << " " << str2 
+		<< charr2[2] << endl;
+
+	cout << "The third letter in " << charr2 << " is "
+		<< charr2[2] << endl;
+
+	cout << "The third letter in " << str2 << " is "
+		<< str2[2] << endl;
+
+	system("pause");
+	return 0;
+}
